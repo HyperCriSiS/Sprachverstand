4e5dbb2290ff7aabcf05ffa5263d0663d507f912
HyperCriSiS <dev.github@w3b.world>
2026-07-26 00:12:27 +0200

    Neue Singular- und Pronomenregeln aktivieren

--- src/rules/index.ts
+++ src/rules/index.ts
@@ -1,14 +1,18 @@
 import type { Rule } from "../core/rule";
 import { binnenIPluralRule } from "./binnen-i";
 import { doubleFormsRule } from "./double-forms";
+import { explicitPronounsRule } from "./explicit-pronouns";
 import { knownPluralSeparatorsRule } from "./known-plural-separators";
 import { mappedPluralSeparatorsRule } from "./mapped-plural-separators";
 import { singularContextRule } from "./singular-context";
+import { singularDoubleFormsRule } from "./singular-double-forms";
 import { specialSingularFormsRule } from "./special-singular-forms";
 
 export const defaultRules: readonly Rule[] = [
   singularContextRule,
   specialSingularFormsRule,
+  singularDoubleFormsRule,
+  explicitPronounsRule,
   doubleFormsRule,
   knownPluralSeparatorsRule,
   mappedPluralSeparatorsRule,
